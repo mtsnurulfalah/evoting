ea243ed82c78506380c4edf2308b06fc678c6b14
mtsnurulfalah <mtssnurulfalah2016@gmail.com>
2026-09-10 22:42:29 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -13,7 +13,7 @@
 
 // URL Google Apps Script Web App
 // Dapatkan dari: GAS Editor > Deploy > Manage Deployments > Web App URL
-window.GAS_URL = 'YOUR_GAS_URL_HERE';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyYpcwtlJLYHX5abMe0S3uFI3pVyRmMNgFQ5PPBJUwTtVs6yWCUiQwfC9BBT6OKw7le7w/exec';
 
 // Konstanta aplikasi (fallback sebelum config dari server dimuat)
 window.APP_CONFIG = {
